0c2c703e65e71185442f86924e1880f6bd2327ac
Encrypted 🇦🇹 <104591921+Desquised@users.noreply.github.com>
2025-03-06 12:06:38 -0300

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ -n “$BASH_VERSION” ]; then
-    CONFIG_FILE=“$HOME/.bashrc”; then
-    SHELL_NAME="Bash”
-elif [ -n “$ZSH_VERSION” ]; then
-    CONFIG_FILE="$HOME/.zshrc”
-    SHELL_NAME="Zsh”
+if [ -n "$BASH_VERSION" ]; then
+    CONFIG_FILE="$HOME/.bashrc"
+    SHELL_NAME="Bash"
+elif [ -n "$ZSH_VERSION" ]; then
+    CONFIG_FILE="$HOME/.zshrc"
+    SHELL_NAME="Zsh"
 else
-    echo “Shell not supported. This script only works in Bash or Zsh.”
+    echo "Shell not supported. This script only works in Bash or Zsh."
     exit 1
 fi
 
-if grep -q “BEGIN AUTO-INSTALL AUTOFIX COMMAND” “$CONFIG_FILE”; then
-    sed -i '/# BEGIN AUTO-INSTALL AUTOFIX COMMAND/,/# END AUTO-INSTALL AUTOFIX COMMAND/d' “$CONFIG_FILE”
-    echo “Autofix block removed from $CONFIG_FILE.”
+if grep -q "BEGIN AUTO-INSTALL AUTOFIX COMMAND" "$CONFIG_FILE"; then
+    sed -i '/# BEGIN AUTO-INSTALL AUTOFIX COMMAND/,/# END AUTO-INSTALL AUTOFIX COMMAND/d' "$CONFIG_FILE"
+    echo "Autofix block removed from $CONFIG_FILE."
 else
-    echo “Autofix block was not found in $CONFIG_FILE. It may already be uninstalled.”
+    echo "Autofix block was not found in $CONFIG_FILE. It may already be uninstalled."
 fi
 
-echo “$SHELL_NAME: Uninstallation completed. Please restart your terminal or run 'source $CONFIG_FILE' to apply the changes.”
+echo "$SHELL_NAME: Uninstallation completed. Please restart your terminal or run 'source $CONFIG_FILE' to apply the changes."
