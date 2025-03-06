9876294e5c0ad5c90d14d0ab57a102721dca8b6b
Encrypted 🇦🇹 <104591921+Desquised@users.noreply.github.com>
2025-03-06 12:14:38 -0300

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -13,9 +13,12 @@ fi
 
 if grep -q "BEGIN AUTO-INSTALL AUTOFIX COMMAND" "$CONFIG_FILE"; then
     sed -i '/# BEGIN AUTO-INSTALL AUTOFIX COMMAND/,/# END AUTO-INSTALL AUTOFIX COMMAND/d' "$CONFIG_FILE"
-    echo "Autofix block removed from $CONFIG_FILE."
+    if [ $? -eq 0 ]; then
+        echo "Autofix block removed from $CONFIG_FILE."
+        echo "$SHELL_NAME: Uninstallation completed. Please restart your terminal or run 'source $CONFIG_FILE' to apply the changes."
+    else
+        echo "Error removing JumpAPT from $CONFIG_FILE."
+    fi
 else
-    echo "Autofix block was not found in $CONFIG_FILE. It may already be uninstalled."
+    echo "JumpAPT was not found in $CONFIG_FILE. It may already be uninstalled."
 fi
-
-echo "$SHELL_NAME: Uninstallation completed. Please restart your terminal or run 'source $CONFIG_FILE' to apply the changes."
